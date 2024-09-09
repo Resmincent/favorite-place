2acd3ff0d8dec7adac602d96a308a08ef763935f
Ifran Hilmi Azhara <itdev.support13@tokiomarine-life.co.id>
2024-09-09 13:11:19 +0700

    done

--- lib/screens/place_list.dart
+++ lib/screens/place_list.dart
@@ -4,13 +4,26 @@ import 'package:maps_ifran/providers/user_places.dart';
 import 'package:maps_ifran/screens/add_place.dart';
 import 'package:maps_ifran/widgets/place_list_item.dart';
 
-class PlaceListScreen extends ConsumerWidget {
+class PlaceListScreen extends ConsumerStatefulWidget {
   const PlaceListScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final userPlace = ref.watch(userPlacesProvider);
+  ConsumerState<PlaceListScreen> createState() {
+    return _PlaceListScreen();
+  }
+}
 
+class _PlaceListScreen extends ConsumerState<PlaceListScreen> {
+  late Future<void> _futurePlace;
+  @override
+  void initState() {
+    super.initState();
+    _futurePlace = ref.read(userPlacesProvider.notifier).loadPlace();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userPlace = ref.watch(userPlacesProvider);
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -31,7 +44,17 @@ class PlaceListScreen extends ConsumerWidget {
         ),
         centerTitle: true,
       ),
-      body: PlaceListItem(places: userPlace),
+      body: FutureBuilder(
+        future: _futurePlace,
+        builder: (context, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : PlaceListItem(
+                    places: userPlace,
+                  ),
+      ),
     );
   }
 }
